1b42d70244aa3356586362e54fc816ea8f1565e6
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2023-11-28 20:12:37 -0300

    feat: yup and formik implemented on login and register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,6 +4,19 @@ import { login } from "../config/firebase";
 import { useUserContext } from "../context/UserContext";
 import { useRedirectActiveUser } from "../hooks/useRedirectActiveUser";
 import { Formik } from "formik";
+import * as yup from 'yup';
+
+const validationSchema = yup.object({
+    email: yup
+        .string()
+        .required("E-mail obrigatório")
+        .email("E-mail inválido"),
+    password: yup
+        .string()
+        .trim()
+        .required("Senha obrigatória")
+        .min(6, "Senha deve ter no mínimo 6 caracteres")
+});
 
 const Login = () => {
     const { user } = useUserContext();
@@ -15,32 +28,44 @@ const Login = () => {
             <h1>Login</h1>
             <Formik
                 initialValues={{ email: '', password: '' }}
-                onSubmit={async ({ email, password }) => {
+                validationSchema={validationSchema}
+                onSubmit={async ({ email, password }, { setSubmitting, setErrors, resetForm }) => {
                     try {
                         const credentialUser = await login({email, password});
                         console.log(credentialUser);
+                        resetForm();
                     }catch(e) {
-                        console.log(e);
+                        if(e.code === 'auth/invalid-login-credentials') {
+                            return setErrors({
+                                email: "Usuário não registrado!", 
+                                password: "Usuário não registrado!" })
+                        }
+                    } finally {
+                        setSubmitting(false)
                     }
                 }}
             >
-                {({ values, handleSubmit, handleChange }) => (
+                {({ values, handleSubmit, handleChange, errors, touched, handleBlur, isSubmitting }) => (
                     <form onSubmit={handleSubmit}>
                         <input 
-                            type="email"
+                            type="text"
                             name="email" 
                             placeholder="Insira seu e-mail" 
                             value={values.email} 
                             onChange={handleChange} 
+                            onBlur={handleBlur}
                         />
+                        {errors.email && touched && errors.email}
                         <input 
-                            type="password" 
+                            type="text" 
                             name="password"
                             placeholder="Insira sua senha" 
                             value={values.password} 
                             onChange={handleChange} 
+                            onBlur={handleBlur}
                         />
-                        <button type="submit">Login</button>
+                        {errors.password && touched && errors.password}
+                        <button type="submit" disabled={isSubmitting}>Login</button>
                     </form>
                 )}
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,28 +1,71 @@
 import { useState } from "react";
 import { register } from "../config/firebase";
+import { Formik } from "formik";
+import * as yup from 'yup';
+
+const validationSchema = yup.object({
+    email: yup
+        .string()
+        .required("E-mail é obrigatório")
+        .email("E-mail inválido"),
+    password: yup
+        .string()
+        .trim()
+        .required("Senha é obrigatória")
+        .min(6, ""),
+});
 
 const Register = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleSubmit = async(e) => {
-        e.preventDefault();
-        try {
-            const credentialUser = await register({ email, password });
-            console.log(credentialUser);
-        } catch(e) {
-            console.log(e);
-        };
-    }
-
     return (
         <>
-        <h1>Register</h1>
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Insira seu e-mail" value={email} onChange={(e) => setEmail(e.target.value)}/>
-            <input type="password" placeholder="Insira sua senha" value={password} onChange={(e) => setPassword(e.target.value)}/>
-            <button type="submit">Register</button>
-        </form>
+            <h1>Register</h1>
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                validationSchema={validationSchema}
+                onSubmit={async ({ email, password }, { setSubmitting, setErrors, resetForm }) => {
+                    try {
+                        const credentialUser = await register({ email, password });
+                        console.log(credentialUser);
+                        resetForm();
+                    } catch (e) {
+                        if (e.code === 'auth/email-already-in-use') {
+                            setErrors({
+                                email: 'Usuário já cadastrado',
+                            })
+                        } else {
+                            setErrors({
+                                email: 'Erro desconhecido'
+                            })
+                        }
+                    } finally {
+                        setSubmitting(false)
+                    }
+                }}
+            >
+                {({ values, handleSubmit, handleChange, errors, touched, isSubmitting, handleBlur }) => (
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            name="email"
+                            placeholder="Insira seu e-mail"
+                            value={values.email}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                        {errors.email && touched && errors.email}
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Insira sua senha"
+                            value={values.password}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                        {errors.password && touched && errors.password}
+                        <button type="submit" disabled={isSubmitting}>Register</button>
+                    </form>
+                )}
+            </Formik>
         </>
     )
 };
